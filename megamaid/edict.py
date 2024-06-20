9be0e5c750e79445b632c61c4a20da5df7318c13
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2024-06-20 07:08:11 -0700

    respect preexisting attribs

--- megamaid/edict.py
+++ megamaid/edict.py
@@ -8,7 +8,11 @@ class Edict(dict):
                 dict.__setitem__(self, key, value)
 
     def __getattr__(self, key):
+        if key in dir(self):
+            return dict.__getattr__(self, key)
         return dict.__getitem__(self, key)
 
     def __setattr__(self, key, value):
-        dict.__setitem__(self, key, value)
+        if key in dir(self):
+            return dict.__setattr__(self, key, value)
+        return dict.__setitem__(self, key, value)
